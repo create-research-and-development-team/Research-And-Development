5e99b3800b971463ab1c3c0df31cffe66558466e
PriestOfFerns <timofeydenin@gmail.com>
2023-07-15 01:22:40 +0300

    Added particles and cooldown for sculk thruster
    stepOn is poopoo garbo, but shouldn't matter since it'll use sculk bullshit

--- common/src/main/java/org/valkyrienskies/vscreate/content/contraptions/sculk/sculkthruster/sculkthruster_block.java
+++ common/src/main/java/org/valkyrienskies/vscreate/content/contraptions/sculk/sculkthruster/sculkthruster_block.java
@@ -1,19 +1,52 @@
 package org.valkyrienskies.vscreate.content.contraptions.sculk.sculkthruster;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.Random;
 
 public class sculkthruster_block extends Block {
+
+    public int cooldown = 0;
     public sculkthruster_block(Properties properties) {
+
         super(properties);
+
     }
 
     @Override
     public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
-        entity.push(0,10,0);
+
+        if (cooldown<=0) {
+            cooldown = 10;
+            Vec3 startPos = entity.position();
+            for (int i = 0; i < 180; i += 2) {
+                level.addParticle(ParticleTypes.END_ROD, startPos.x, startPos.y + 0.1, startPos.z, Math.cos(i) * 0.2 * Math.random(), 0.1, Math.sin(i) * 0.2 * Math.random());
+            }
+
+            entity.push(0, 2, 0);
+
+        }
         super.stepOn(level, pos, state, entity);
+    }
+
+
+
+    // TODO: Make Cooldown less shit, I refuse to believe this is the best way to
+    // make a cooldown. Ticking shit bad for lag and all that
+    @Override
+    public void animateTick(BlockState state, Level level, BlockPos pos, Random random) {
+        System.out.println(cooldown);
+        if (cooldown>0) {
+            cooldown-=1;
+        }
+
+        super.animateTick(state, level, pos, random);
     }
 }
