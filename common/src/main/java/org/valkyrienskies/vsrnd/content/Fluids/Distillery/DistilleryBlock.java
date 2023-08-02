9e491124a45d7ceec8ae7eade14a9dc1f3e6445d
PriestOfFerns <timofeydenin@gmail.com>
2023-08-02 20:47:02 +0300

    minor spelling mistake

--- common/src/main/java/org/valkyrienskies/vsrnd/content/Fluids/Distillery/DistilleryBlock.java
+++ common/src/main/java/org/valkyrienskies/vsrnd/content/Fluids/Distillery/DistilleryBlock.java
@@ -2,8 +2,8 @@ package org.valkyrienskies.vsrnd.content.Fluids.Distillery;
 
 import com.simibubi.create.content.processing.basin.BasinBlock;
 
-public class DistilleyBlock extends BasinBlock {
-    public DistilleyBlock(Properties p_i48440_1_) {
+public class DistilleryBlock extends BasinBlock {
+    public DistilleryBlock(Properties p_i48440_1_) {
         super(p_i48440_1_);
     }
 }
