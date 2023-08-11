16f261e001bfbc6c2ab17e9081cba8b1bb3bba98
PriestOfFerns <timofeydenin@gmail.com>
2023-08-11 23:30:41 +0300

    deleted config

--- common/src/main/java/org/valkyrienskies/vsrnd/content/World/Rutile/RutileFeatureConfig.java
+++
@@ -1,6 +0,0 @@
-package org.valkyrienskies.vsrnd.content.World.Rutile;
-
-import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
-
-public class RutileFeatureConfig implements FeatureConfiguration {
-}
