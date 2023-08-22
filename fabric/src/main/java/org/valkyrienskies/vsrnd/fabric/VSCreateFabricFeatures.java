6d2fc75c6ffd21037bb9a9550fe3ad36e48ef711
TonyGamer <tomblackaz@gmail.com>
2023-08-21 23:25:30 -0700

    Reformatted Fabric Features

--- fabric/src/main/java/org/valkyrienskies/vsrnd/fabric/VSCreateFabricFeatures.java
+++ fabric/src/main/java/org/valkyrienskies/vsrnd/fabric/VSCreateFabricFeatures.java
@@ -1,18 +1,11 @@
 package org.valkyrienskies.vsrnd.fabric;
 
-
-
-import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.data.BuiltinRegistries;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 import net.minecraft.world.level.levelgen.placement.CountPlacement;
 import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
@@ -21,13 +14,12 @@ import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import org.valkyrienskies.vsrnd.VSCreateMod;
 import org.valkyrienskies.vsrnd.content.World.Rutile.RutileFeature;
 
-import javax.annotation.Resource;
 import java.util.Arrays;
 
 public class VSCreateFabricFeatures {
 
     private static ConfiguredFeature<?, ?> RUTILE_CLUSTER_CONFIGURED = new ConfiguredFeature
-            (new RutileFeature(NoneFeatureConfiguration.CODEC), new NoneFeatureConfiguration());
+            (new RutileFeature(NoneFeatureConfiguration.CODEC), NoneFeatureConfiguration.INSTANCE);
 
     public static PlacedFeature RUTILE_CLUSTER_PLACED = new PlacedFeature(
             Holder.direct(RUTILE_CLUSTER_CONFIGURED),
@@ -40,10 +32,17 @@ public class VSCreateFabricFeatures {
 
     static public void init() {
         Registry.register(BuiltinRegistries.CONFIGURED_FEATURE,
-                new ResourceLocation(VSCreateMod.MOD_ID, "Rutile_Cluster"), RUTILE_CLUSTER_CONFIGURED);
-        Registry.register(BuiltinRegistries.PLACED_FEATURE, new ResourceLocation("tutorial", "overworld_wool_ore"),
-                RUTILE_CLUSTER_PLACED);
-        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Decoration.UNDERGROUND_ORES,
-                ResourceKey.create(Registry.PLACED_FEATURE_REGISTRY,new ResourceLocation(VSCreateMod.MOD_ID, "Rutile_Cluster"))  );
+                          new ResourceLocation(VSCreateMod.MOD_ID, "rutile_cluster"),
+                          RUTILE_CLUSTER_CONFIGURED);
+        Registry.register(BuiltinRegistries.PLACED_FEATURE,
+                          new ResourceLocation(VSCreateMod.MOD_ID, "rutile_cluster"),
+                          RUTILE_CLUSTER_PLACED);
+        // TODO: Fix crash related to adding rutile clusters
+        /*BiomeModifications.addFeature(
+                BiomeSelectors.foundInOverworld(),
+                GenerationStep.Decoration.UNDERGROUND_ORES,
+                ResourceKey.create(
+                        Registry.PLACED_FEATURE_REGISTRY,
+                        new ResourceLocation(VSCreateMod.MOD_ID, "rutile_cluster")));*/
     }
 }
