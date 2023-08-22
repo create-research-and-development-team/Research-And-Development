da0923e2d17644a7b4da772a60724b1252ca5f42
TonyGamer <tomblackaz@gmail.com>
2023-08-21 23:30:58 -0700

    Fixed modeEventBus

--- forge/src/main/java/org/valkyrienskies/vsrnd/forge/VSCreateModForge.java
+++ forge/src/main/java/org/valkyrienskies/vsrnd/forge/VSCreateModForge.java
@@ -1,15 +1,16 @@
 package org.valkyrienskies.vsrnd.forge;
 import com.simibubi.create.AllParticleTypes;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLLoader;
 import org.valkyrienskies.vsrnd.*;
 import org.valkyrienskies.vsrnd.forge.integration.cc_tweaked.VSCreateForgePeripheralProviders;
-
-import static org.valkyrienskies.vsrnd.RNDRegistrate.REGISTRATE;
-
-
 
 public class VSCreateModForge {
     boolean happenedClientSetup = false;
@@ -44,8 +45,9 @@ public class VSCreateModForge {
         VSCreatePackets.init();
 
         DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
-            FMLJavaModLoadingContext.get()
-                    .getModEventBus().addListener(VSCreateForgeParticles::register);
+            IEventBus modEventBus = FMLJavaModLoadingContext.get()
+                    .getModEventBus();
+            modEventBus.addListener(VSCreateForgeParticles::register);
 
             // In create itself they do it FMLClientSetupEvent this does not work (what a scam)
             // It prob gets staticly loaded earlier and well yhea...
